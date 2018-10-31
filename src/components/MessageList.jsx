2b683a78db6134384e393a182294b401cdad8887
s0njablade <sonjita.chacon@gmail.com>
2018-10-31 11:10:28 -0600

    basic toolbar component

---
+++ src/components/MessageList.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class MessageList extends Component {
+  render() {
+    return (
+      <div className="MessageList">
+        
+      </div>
+    );
+  }
+}
+
+export default MessageList;
